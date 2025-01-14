9cb86a72c8d6029a90dbf07913b85a1e19be3cb0
Muziljohn <muzilhassan9@gmail.com>
2025-01-14 13:39:10 +0500

    setuped the project and routing

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="account" element={<Account />} />
+          <Route path="Bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="users" element={<Users />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
